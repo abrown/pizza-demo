7a71a7e2aecba64d997b33b1e8c994f9a85133a3
Andrew Brown <andrew.brown@intel.com>
2016-07-21 20:42:08 -0700

    Add static order items and success page

--- app/src/main/java/com/example/abrown/pizza/SuccessActivity.java
+++ app/src/main/java/com/example/abrown/pizza/SuccessActivity.java
@@ -7,15 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ConfirmActivity extends AppCompatActivity {
+public class SuccessActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        setContentView(R.layout.activity_success);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        WidgetHelper.goTo(this, SuccessActivity.class, R.id.fab);
     }
 }
